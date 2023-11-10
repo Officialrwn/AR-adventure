ae241863e9312ca4dc077a09db804857971cd102
leotran@student.hive.fi <vanleotran@gmail.com>
2023-11-10 20:33:03 +0200

    added sample code from mapbox

--- client/src/pages/MapPage/MapPage.jsx
+++ client/src/pages/MapPage/MapPage.jsx
@@ -13,18 +13,31 @@ const MapPage = () => {
 
 	useEffect(() => {
 		if (map.current) return; // initialize map only once
-		map.current = new mapboxgl.Map({
-		container: mapContainer.current,
-		style: 'mapbox://styles/mapbox/streets-v12',
-		center: [lng, lat],
-		zoom: zoom
+			map.current = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: 'mapbox://styles/mapbox/streets-v12',
+			center: [lng, lat],
+			zoom: zoom
+		});
+
+		map.current.on('move', () => {
+			setLng(map.current.getCenter().lng.toFixed(4));
+			setLat(map.current.getCenter().lat.toFixed(4));
+			setZoom(map.current.getZoom().toFixed(2));
 		});
 	});
 	return (
-		<>
-			<h1>Map Page</h1>
-			<div ref={mapContainer} className="map-container" />
-		</>
+		<div className="main">
+			<div className="header">
+				<h1>Map Page</h1>
+			</div>
+			<div className="map">
+				<div className="sidebar">
+					Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+				</div>
+				<div ref={mapContainer} className="map-container" />
+			</div>
+		</div>
 	)
 }
 
