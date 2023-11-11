e6a25ad1107b4ed5a5ea5c06f7885dd9dffc521b
Binh Bui <binh@ewq.zone>
2023-11-11 15:33:39 +0200

    Added view logic to Map

--- client/src/pages/MapPage/MapPage.jsx
+++ client/src/pages/MapPage/MapPage.jsx
@@ -1,33 +1,40 @@
-import React, { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
-import { parks } from '../../data/parks';
-import CustomMarker from '../../components/CustomMarker/CustomMarker';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import './index.css';
+import React, { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { parks } from "../../data/parks";
+import CustomMarker from "../../components/CustomMarker/CustomMarker";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./index.css";
+import { useView } from "../../contexts/ViewContext";
 
 const MapPage = () => {
-	const [activeMarker, setActiveMarker] = useState(null);
-	const defaultViewPort = {
+  const { setViewingQuests, setMenuOpen } = useView();
+
+  const [activeMarker, setActiveMarker] = useState(null);
+  const defaultViewPort = {
     latitude: 60.17000701866055,
     longitude: 24.93737401348124,
-    width: '100vw',
-    height: '100vh',
+    width: "100vw",
+    height: "100vh",
     zoom: 12,
   };
   const [viewport, setViewport] = useState(defaultViewPort);
   const markers = parks;
 
   const handleOnMarkerClick = (marker) => {
+    setViewingQuests(true);
+    setMenuOpen(true);
+
     console.log(marker.title);
-		const newViewPort = {
-			latitude: marker.latitude,
+    const newViewPort = {
+      latitude: marker.latitude,
       longitude: marker.longitude,
       zoom: 14,
-		}
-		const prevActiveMarker = activeMarker === marker.title ? null : marker.title;
-		const prevViewPort = prevActiveMarker ? newViewPort : defaultViewPort;
-		setViewport(prevViewPort);
-		setActiveMarker(prevActiveMarker);
+    };
+    const prevActiveMarker =
+      activeMarker === marker.title ? null : marker.title;
+    const prevViewPort = prevActiveMarker ? newViewPort : defaultViewPort;
+    setViewport(prevViewPort);
+    setActiveMarker(prevActiveMarker);
   };
 
   // const mapStyle = "mapbox://styles/leighhalliday/cjufmjn1r2kic1fl9wxg7u1l4";
@@ -50,7 +57,11 @@ const MapPage = () => {
             offsetLeft={-20}
             offsetTop={-10}
           >
-            <CustomMarker isActive={activeMarker === marker.title} title={marker.title} onClick={() => handleOnMarkerClick(marker)} />
+            <CustomMarker
+              isActive={activeMarker === marker.title}
+              title={marker.title}
+              onClick={() => handleOnMarkerClick(marker)}
+            />
           </Marker>
         ))}
       </ReactMapGL>
