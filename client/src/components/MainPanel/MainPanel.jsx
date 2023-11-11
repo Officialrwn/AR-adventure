2a968d03cb7edb9138c10fed08c582d6d3c7034c
Binh Bui <binh@ewq.zone>
2023-11-11 14:01:50 +0200

    Added main panel

--- client/src/components/MainPanel/MainPanel.jsx
+++ client/src/components/MainPanel/MainPanel.jsx
@@ -3,6 +3,7 @@ import React, { useRef, useState } from "react";
 import BurgerIcon from "../BurgerIcon/BurgerIcon";
 import Menu from "../Menu/Menu";
 import { useOnClickOutside } from "../../hooks/useOnClickOutside";
+import { StyledMainPanel } from "./MainPanel.styled";
 
 const MainPanel = () => {
   const [open, setOpen] = useState(false);
@@ -13,7 +14,9 @@ const MainPanel = () => {
   return (
     <div ref={node}>
       <BurgerIcon open={open} setOpen={setOpen} />
-      <Menu open={open} />
+      <StyledMainPanel open={open}>
+        <Menu open={open} />
+      </StyledMainPanel>
     </div>
   );
 };
---
+++ client/src/components/MainPanel/MainPanel.styled.js
@@ -0,0 +1,22 @@
+// MainPanel.styled.js
+import styled from "styled-components";
+
+export const StyledMainPanel = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: ${({ theme }) => theme.primaryLight};
+  text-align: left;
+  padding: 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 9;
+  height: 100vh;
+  transition: transform 0.3s ease-in-out;
+  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    width: 100%;
+  }
+`;
