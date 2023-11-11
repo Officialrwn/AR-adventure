1c7dccb3646f3d43e3eb62f67f1ca94cb9dc8082
Binh Bui <binh@ewq.zone>
2023-11-11 11:51:30 +0200

    Added hamburger menu skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,32 @@ import HomePage from "./pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CharacterPage } from "./pages/CharacterPage";
 import MapPage from "./pages/MapPage/MapPage";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { GlobalStyles } from "./global";
+import { useState } from "react";
+import { Burger, Menu } from "./components";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/map" element={<MapPage />} />
-        <Route path="/character" element={<CharacterPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div>
+          <Burger open={open} setOpen={setOpen} />
+          <Menu  open={open} setOpen={setOpen} />
+        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/map" element={<MapPage />} />
+            <Route path="/character" element={<CharacterPage />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    </ThemeProvider>
   );
 }
 
