038cc5479078d7b8cfd5d6d33f68200ae6afdeb7
Binh Bui <binh@ewq.zone>
2023-11-11 10:46:16 +0200

    Added styling

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MapPage from "./pages/MapPage";
+import { CharacterPage } from "./pages/CharacterPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/character" element={<CharacterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
