5e27c272fbab1cc5c8c7cef02eb9f61d21552326
Binh Bui <binh@ewq.zone>
2023-11-11 10:54:13 +0200

    Added mappage route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CharacterPage } from "./pages/CharacterPage";
+import MapPage from "./pages/MapPage/MapPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/map" element={<MapPage />} /> */}
+        <Route path="/map" element={<MapPage />} />
         <Route path="/character" element={<CharacterPage />} />
       </Routes>
     </BrowserRouter>
