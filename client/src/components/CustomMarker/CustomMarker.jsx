484b294225eb2b0e93861bdb03a41f8c4f3a19e7
leotran@student.hive.fi <vanleotran@gmail.com>
2023-11-11 14:17:37 +0200

    added background color on select

--- client/src/components/CustomMarker/CustomMarker.jsx
+++ client/src/components/CustomMarker/CustomMarker.jsx
@@ -1,16 +1,35 @@
-import icon from '../../assets/questicon.png'
-import './index.css'
+import React, { useState } from 'react';
+import icon from '../../assets/questicon.png';
+import './index.css';
 
-const CustomMarker = ({title, onClick}) => {
-	const iconStyle = {
-		width: "20px",
-		height: "20px"
+const CustomMarker = ({ title, onClick }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const iconStyle = {
+    width: "20px",
+    height: "20px"
   };
-	return (
-		<div className="custom-marker" onClick={onClick}>
-			<img src={icon} alt="quest icon"  style={iconStyle} />
-			<p>{title} Quest</p>
-		</div>
-	)};
+
+  const markerStyle = {
+    border: '2px solid green',
+		backgroundColor: 'green',
+		color: 'white',
+    padding: '5px',
+    borderRadius: '5px',
+    cursor: 'pointer'
+  };
+	
+	const defaultStyle = {
+		fontWeight: 'bold',
+		color: 'green'
+	}
+
+  return (
+    <div className="custom-marker" style={ clicked ? markerStyle : defaultStyle } onClick={() => { setClicked(!clicked); onClick(); }}>
+      <img src={icon} alt="quest icon" style={iconStyle} />
+      <p>{title} Quest</p>
+    </div>
+  );
+};
 
 export default CustomMarker;
