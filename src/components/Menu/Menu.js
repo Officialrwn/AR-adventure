3fba86d99ddf03fcb4d7a54af9c6a5803f253e4e
Binh Bui <binh@ewq.zone>
2023-11-12 03:24:52 +0200

    Show hero directly in menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,18 +2,20 @@ import React from "react";
 import { bool } from "prop-types";
 import { StyledMenu } from "./Menu.styled";
 import { useView } from "../../contexts/ViewContext";
+import Hero from "../Hero/Hero";
 
 const Menu = ({ open }) => {
   const { setPanelView } = useView();
 
   return (
     <StyledMenu open={open}>
-      <div className="menu-item" onClick={() => setPanelView("hero")}>
+      {/* <div className="menu-item" onClick={() => setPanelView("hero")}>
         <span role="img" aria-label="about us">
           &#x1f481;&#x1f3fb;&#x200d;&#x2642;&#xfe0f;
         </span>
         Hero
-      </div>
+      </div> */}
+      <Hero />
       <div className="menu-item" onClick={() => setPanelView("quests")}>
         <span role="img" aria-label="price">
           &#128214;
